8a2e93eed7d5613c96f250ef037897a1f4ec106b
John Selkirk <john.selkirk@tctechnology.com>
2016-02-17 17:18:02 -0500

    correct url this time

--- Silverlight/tools/chocolateyInstall.ps1
+++ Silverlight/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 # http://www.microsoft.com/about/legal/en/us/Copyright/Default.aspx  #copyright
 $url = 'http://go.microsoft.com/fwlink/?LinkID=229320'
-$url64bit = 'http://go.microsoft.com/fwlink/?LinkID=229324'
+$url64bit = 'http://go.microsoft.com/fwlink/?linkid=149156'
 Install-ChocolateyPackage 'Silverlight' 'exe' '/q' "$url" "$url64bit"  -validExitCodes @(0)
