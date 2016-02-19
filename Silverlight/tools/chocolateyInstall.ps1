439a1f34d152d148c9e880903152207c3773b519
John Selkirk <john.selkirk@tctechnology.com>
2016-02-19 09:27:07 -0500

    use direct links

--- Silverlight/tools/chocolateyInstall.ps1
+++ Silverlight/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 # http://www.microsoft.com/about/legal/en/us/Copyright/Default.aspx  #copyright
-$url = 'http://go.microsoft.com/fwlink/?LinkID=229320'
-$url64bit = 'http://go.microsoft.com/fwlink/?linkid=149156'
+$url = 'https://download.microsoft.com/download/5/3/D/53D3880B-25F8-4714-A4AC-E463A492F96E/41212.00/Silverlight.exe'
+$url64bit = 'https://download.microsoft.com/download/5/3/D/53D3880B-25F8-4714-A4AC-E463A492F96E/41212.00/Silverlight_x64.exe'
 Install-ChocolateyPackage 'Silverlight' 'exe' '/q' "$url" "$url64bit"  -validExitCodes @(0)
