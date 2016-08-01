11b92c5a13e1f6ece33aa6c8db2868d72d688788
John Selkirk <john.selkirk@tctechnology.com>
2016-08-01 11:40:36 -0400

    upgraded to 5.1.50428.0

--- Silverlight/tools/chocolateyInstall.ps1
+++ Silverlight/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 # http://www.microsoft.com/about/legal/en/us/Copyright/Default.aspx  #copyright
-$url = 'https://download.microsoft.com/download/5/3/D/53D3880B-25F8-4714-A4AC-E463A492F96E/41212.00/Silverlight.exe'
-$url64bit = 'https://download.microsoft.com/download/5/3/D/53D3880B-25F8-4714-A4AC-E463A492F96E/41212.00/Silverlight_x64.exe'
+$url = 'https://download.microsoft.com/download/1/F/6/1F637DB3-8EF9-4D96-A8F1-909DFD7C5E69/50428.00/Silverlight.exe'
+$url64bit = 'https://download.microsoft.com/download/1/F/6/1F637DB3-8EF9-4D96-A8F1-909DFD7C5E69/50428.00/Silverlight_x64.exe'
 Install-ChocolateyPackage 'Silverlight' 'exe' '/q' "$url" "$url64bit"  -validExitCodes @(0)
