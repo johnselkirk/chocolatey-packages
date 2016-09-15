3ac6d856695949d601c38d19b4942bf56aedcb7a
John Selkirk <john.selkirk@tctechnology.com>
2016-09-15 13:43:41 -0400

    upgraded to 5.1.50709.0

--- Silverlight/release-notes.md
+++ Silverlight/release-notes.md
@@ -1,5 +1,9 @@
 #Release Notes#
 
+##5.1.50709.0 (September 13, 2016)
+
+Upgraded to version [5.1.50709.0](https://www.microsoft.com/getsilverlight/locale/en-us/html/Microsoft%20Silverlight%20Release%20History.htm#SL_5_1_50709)
+
 ##5.1.50428.0 (June 21, 2016)
 
 Upgraded to version [5.1.50428.0](https://www.microsoft.com/getsilverlight/locale/en-us/html/Microsoft%20Silverlight%20Release%20History.htm#SL_5_1_50428)
--- Silverlight/tools/chocolateyInstall.ps1
+++ Silverlight/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 # http://www.microsoft.com/about/legal/en/us/Copyright/Default.aspx  #copyright
-$url = 'https://download.microsoft.com/download/1/F/6/1F637DB3-8EF9-4D96-A8F1-909DFD7C5E69/50428.00/Silverlight.exe'
-$url64bit = 'https://download.microsoft.com/download/1/F/6/1F637DB3-8EF9-4D96-A8F1-909DFD7C5E69/50428.00/Silverlight_x64.exe'
+$url = 'https://download.microsoft.com/download/7/7/6/7765A6A5-4B02-41DE-B7AF-067C92C581BD/50709.00/Silverlight.exe'
+$url64bit = 'https://download.microsoft.com/download/7/7/6/7765A6A5-4B02-41DE-B7AF-067C92C581BD/50709.00/Silverlight_x64.exe'
 Install-ChocolateyPackage 'Silverlight' 'exe' '/q' "$url" "$url64bit"  -validExitCodes @(0)
